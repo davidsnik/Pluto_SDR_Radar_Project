3c5cfe05617b66ea72002bd632a8a413de80ecc0
David <D.Snik@student.tudelft.nl>
2025-06-10 16:13:26 +0200

    fixed issues with plotting code and signals

--- PlutoSDR.py
+++ PlutoSDR.py
@@ -11,9 +11,8 @@ class PlutoSDR:
             my_sdr.sample_rate = int(sample_rate)
             my_sdr.tx_rf_bandwidth = int(sample_rate)
             my_sdr.rx_rf_bandwidth = int(sample_rate)
-            my_sdr.rx_lo = int(tx_center_freq)
-            my_sdr.tx_lo = int(rx_center_freq)
-            my_sdr.rx_output_type    
+            my_sdr.rx_lo = int(rx_center_freq)
+            my_sdr.tx_lo = int(tx_center_freq)
             my_sdr.rx_enabled_channels = [0]
             sample_rate = int(my_sdr.sample_rate)
             my_sdr.gain_control_mode_chan0 = "manual"
@@ -22,13 +21,15 @@ class PlutoSDR:
 
             my_sdr.tx_enabled_channels = [0]
             my_sdr.tx_hardwaregain_chan0 = int(tx_gain)
-            
+
             frame_length_samples = rx_samples_per_frame
-            
+
             N_rx = int(1 * frame_length_samples)
             my_sdr.rx_buffer_size = N_rx
 
             self.pluto_interface = my_sdr
+        else:
+            self.pluto_interface = None
 
         self.Pluto_IP = PlutoIP
         self.sample_rate = sample_rate
@@ -42,7 +43,7 @@ class PlutoSDR:
         self.chirp_type = None
         self.chirp_amplitude = None
         self.chirp_bandwidth = None
-        self.chirp_duration = None
+        self.chirp_duration_ms = None
         self.tx_iq = None
         self.rx_iq = None
         self.s_beat = None
@@ -51,29 +52,49 @@ class PlutoSDR:
         self.s_beat_signal_coherent_integration = None
         self.s_beat_freq_coherent_integration = None
 
-    def set_waveform(self, chirp_type, chirp_amplitude, chirp_bandwidth, chirp_duration):
-        sample_period = 1/self.sample_rate
+    def set_waveform(self, chirp_type, chirp_amplitude, chirp_bandwidth, chirp_duration_ms):
+        sample_period = 1 / self.sample_rate
+        duration_seconds = chirp_duration_ms * 1e-3  # Convert ms to seconds
 
-        time = np.arange(0, chirp_duration + sample_period, sample_period)
+        time = np.arange(0, duration_seconds, sample_period)
+
+        window = np.hanning(len(time))
 
         match chirp_type:
             case "SawtoothWave":
-                chirp_slope = chirp_bandwidth/(chirp_duration*(10**-3))
-                self.tx_iq = chirp_amplitude * np.exp(-1j*np.pi*chirp_slope*(time**2))
+                chirp_slope = chirp_bandwidth / duration_seconds  # units: Hz/s
+                phase = 2 * np.pi * (0.5 * chirp_slope * (time**2))
+                self.tx_iq = chirp_amplitude * np.exp(1j * phase) * window
+
             case "TriangularWave":
-                chirp_slope = chirp_bandwidth/((chirp_duration*(10**-3))/2)
-                self.tx_iq = chirp_amplitude * np.concatonate(
-                    np.exp(-1j*np.pi*chirp_slope*((time[:len(time)//2])**2)),
-                    np.exp(-1j*np.pi*(-chirp_slope)*((time[len(time)//2:])**2))
-                    )
-            case _: 
+                half = len(time) // 2
+                t_up = time[:half]
+                t_down = time[half:]
+
+                chirp_slope = chirp_bandwidth / (duration_seconds / 2)  # Hz/s for each half
+
+                # Up-chirp: 0 -> bandwidth
+                phase_up = 2 * np.pi * (0.5 * chirp_slope * t_up**2)
+                # Down-chirp: bandwidth -> 0, ensure phase continuity
+                tau = t_down - t_up[-1]
+                phase_down = phase_up[-1] + 2 * np.pi * (
+                    chirp_bandwidth * tau - 0.5 * chirp_slope * tau**2
+                )
+
+                self.tx_iq = chirp_amplitude * np.concatenate([
+                    np.exp(1j * phase_up),
+                    np.exp(1j * phase_down)
+                ]) * window
+
+            case _:
                 return None
 
         self.chirp_time_axis = time
         self.chirp_type = chirp_type
         self.chirp_amplitude = chirp_amplitude
-        self.chirp_bandwidth = chirp_bandwidth # in hz
-        self.chirp_duration = chirp_duration # in ms
+        self.chirp_bandwidth = chirp_bandwidth  # in Hz
+        self.chirp_duration_ms = chirp_duration_ms  # in ms
+
 
     def start_transmission(self):
         if self.chirp_type != None:
@@ -111,14 +132,14 @@ class PlutoSDR:
         pulses_to_add = []
         for i in range(number_of_pulses_to_add):
             time.sleep((pulse_sleep_time_ms*(10**-3))/2)
-            pulses_to_add.append(get_beat_signal())
+            pulses_to_add.append(self.get_beat_signal())
             time.sleep((pulse_sleep_time_ms*(10**-3))/2)
 
         if len(self.s_beat_buffer) >= pulse_buffer_size:
             self.s_beat_buffer = self.s_beat_buffer[pulse_buffer_size-number_of_pulses_to_add+(len(self.s_beat_buffer)-pulse_buffer_size):]
         self.s_beat_buffer.extend(pulses_to_add)
 
-        min_len = min(len(s_beat_sig) for s_beat_sig in self.s_beat_buffer)
+        min_len = min([len(s_beat_sig) for s_beat_sig in self.s_beat_buffer])
 
         s_beat_signal_coherent_integration = np.mean([s_beat_sig[:min_len] for s_beat_sig in self.s_beat_buffer])
         self.s_beat_signal_coherent_integration = s_beat_signal_coherent_integration
--- main.py
+++ main.py
@@ -8,32 +8,48 @@ centerFrequency = 2.5e9 # in hz
 tx_gain = -20 # in db
 rx_gain = 40 # in db
 rx_frame_duration = 1 # in ms
-rx_samples_per_frame = int((rx_frame_duration*(10**-3))/sample_rate)
+rx_samples_per_frame = int((rx_frame_duration*(10**-3))*sample_rate)
 
-chirp_type = "SawtoothWave" # Options: "SawtoothWave", "TriangularWave"
+chirp_type = "TriangularWave" # Options: "SawtoothWave", "TriangularWave"
 chirp_amplitude = (2**12)
 chirp_bandwidth = 30e6 # hz
 chirp_duration = rx_frame_duration # ms
 
 sdr_obj = PlutoSDR(PlutoIP, sample_rate, centerFrequency, centerFrequency, rx_gain, tx_gain, rx_samples_per_frame, skip_pluto_configuration=True)
 sdr_obj.set_waveform(chirp_type, chirp_amplitude, chirp_bandwidth, chirp_duration)
-#sdr_obj.start_transmission()
-#received_data = sdr_obj.receive_data()
 
-f, t, Z = stft(sdr_obj.tx_iq, fs = sample_rate, nperseg = 256, return_onesided = True)
-# pos_freq = f>=0
-# f_pos = f[pos_freq]
-# Z_pos = Z[pos_freq]
-plt.figure(figsize = (10,4))
-plt.pcolormesh(t,f,np.abs(Z)/np.max(np.abs(Z)), shading = 'gouraud')
+nperseg = 1024  # Increased from 256
+f, t, Zxx = stft(sdr_obj.tx_iq, fs=sample_rate, nperseg=nperseg, return_onesided=False)
+
+magnitude = np.abs(Zxx)
+magnitude_db = 20 * np.log10(magnitude + 1e-12)
+
+positive_freqs = f >= 0
+f_pos = f[positive_freqs] / 1e6  # Convert to MHz
+Z_pos = magnitude_db[positive_freqs, :]
+
+plt.figure(figsize=(10, 4))
+plt.imshow(
+    Z_pos,
+    aspect='auto',
+    origin='lower',
+    extent=[t[0]*1e3, t[-1]*1e3, f_pos[0], f_pos[-1]],  # Time in ms, freq in MHz
+    cmap='viridis',
+    vmin=np.percentile(Z_pos, 10),
+    vmax=np.percentile(Z_pos, 90)
+)
+plt.xlabel('Time [ms]')
+plt.ylabel('Frequency [MHz]')
+plt.colorbar(label='Magnitude [dB]')
+plt.title(f'Spectrogram ({chirp_type}, Bandwidth={chirp_bandwidth/1e6} MHz)')
 plt.tight_layout()
-plt.show()
+plt.savefig("spectrogram_corrected.png")
 
 # %%%%%%%%%%%%%%%%%%%% Pluto's parameters configuration %%%%%%%%%%%%%%%%%%%%%
 
-# % If you want repeatable alignment between transmit and receive then the 
+# % If you want repeatable alignment between transmit and receive then the
 # % rx_lo, tx_lo and sample_rate can only be set once after power up. If you
-# % want to change the following parameters' values after the first run you 
+# % want to change the following parameters' values after the first run you
 # % must reboot the Pluto (disconnect and reconnect it)
 
 # Pluto_IP = '192.168.2.1';
@@ -51,17 +67,17 @@ plt.show()
 # B = 30e6;       % Chirp bandwidth (Hz)
 # T = 100e-6;       % Chirp duration (s)
 # f0 = 2.5e9;      % Carrier frequency of the RF signal (Hz)
-# fs = PlutoSamprate;     % Sampling rate of all the signals in this simulation (Hz) 
+# fs = PlutoSamprate;     % Sampling rate of all the signals in this simulation (Hz)
 # c = 3e8;        % Speed of light (m/s)
 
 
 # chirp_duration = 1; % Chirp duration (ms)
-# % t = 1/fs:1/fs:chirp_duration*1e-3;  
-# t = single(0:1/fs:(chirp_duration*1e-3 ));  
+# % t = 1/fs:1/fs:chirp_duration*1e-3;
+# t = single(0:1/fs:(chirp_duration*1e-3 ));
 
-# % t = 0:1/fs:(T-1/fs); 
+# % t = 0:1/fs:(T-1/fs);
 # k = B / T;     % Chirp slope defined as ratio of bandwidth over duration
 # sig_A = exp(1j*2*pi*(0.5*k*t.^2));
 
-# tx_waveform = 2^12.*sig_A.';  % # The PlutoSDR expects samples to be between 
+# tx_waveform = 2^12.*sig_A.';  % # The PlutoSDR expects samples to be between
 # %                           -2^14 and +2^14, not -1 and +1 like some SDRs
